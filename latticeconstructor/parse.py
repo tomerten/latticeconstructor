e558b8df3b58036fc2ee2698c7f57366229dee1d
Tom Mertens <your.email@whatev.er>
2021-05-18 21:19:49 +0200

    added lark parser for madx seq

--- latticeconstructor/parse.py
+++ latticeconstructor/parse.py
@@ -8,10 +8,41 @@ A module
 
 """
 
+from pathlib import Path
 from typing import Tuple, Union
 
+import pandas as pd
+from lark import Lark, Transformer, v_args
+from lark.exceptions import LarkError
 from latticejson.convert import FROM_ELEGANT, TO_MADX
-from latticejson.parse import parse_elegant, parse_madx
+from latticejson.parse import AbstractLatticeFileTransformer, ArithmeticTransformer, parse_elegant
+
+BASE_DIR = Path(__file__).resolve().parent
+
+with (BASE_DIR / "madx.lark").open() as file:
+    MADX_PARSER = Lark(file, parser="lalr", maybe_placeholders=True)
+    file.seek(0)
+    ARITHMETIC_PARSER = Lark(file, parser="lalr", start="start_artih")
+
+
+@v_args(inline=True)
+class MADXTransformer(ArithmeticTransformer, AbstractLatticeFileTransformer):
+    def sequence(self, name, *items):
+        *attributes, elements = items
+        self.lattices[name.upper()] = elements
+        self.commands.append(("name", name))
+
+    def seq_element(self, name, value):
+        return name.upper(), value
+
+    def seq_elements(self, *elements):
+        return list(elements)
+
+
+def parse_madx(string: str):
+    tree = MADX_PARSER.parse(string)
+    return MADXTransformer().transform(tree)
+
 
 _CONVERSION_DICT = {
     "KQUAD": "QUADRUPOLE",
@@ -34,15 +65,12 @@ def parse_from_string(string: str, ftype: str = "lte") -> Tuple[Union[str, None]
     Parameters
     ----------
     string : str
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    Lattice string.
 
     ftype: optional, str, default = 'lte'
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    format type
 
     Returns
     -------
     Tuple[Union[str,None],dict, list]
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    name, element definitions dict, element ordered list
 
     """
     assert ftype.lower() in ["lte", "madx"]
@@ -57,7 +85,7 @@ def parse_from_string(string: str, ftype: str = "lte") -> Tuple[Union[str, None]
     cdict = {}
     for command in latdata.get("commands"):
         try:
-            cdict[command[0]] = command[1] if len(command[1]) == 1 else list(command[1])
+            cdict[command[0]] = command[1] if len(command) == 2 else list(command[1])
         except Exception:
             cdict[command[0]] = ""
 
@@ -99,12 +127,21 @@ def parse_from_string(string: str, ftype: str = "lte") -> Tuple[Union[str, None]
     # hack below can be avoided
     if bool(lattice):
         lattice = list(flatten(lattice))
-        lattice = [el.upper() for el in lattice]
+        if isinstance(lattice[0], tuple):
+            positions = pd.DataFrame(lattice, columns=["name", "at"])
+            lattice = [el[0].upper() for el in lattice]
+            lattice_name = cdict["name"]
+        else:
+            positions = None
+            lattice = [el.upper() for el in lattice]
+            lattice_name = None
     else:
         cdict.pop("ENDSEQUENCE")
         lattice = [el.upper() for el in list(cdict.keys())]
+        positions = [{k.upper(): float(v[1])} for k, v in cdict.items()]
 
     # attempt to load lattice name
-    lattice_name = cdict.get("use", None)
+    if ftype == "lte":
+        lattice_name = cdict.get("use", None)
 
-    return lattice_name, definitions, lattice
+    return lattice_name, positions, definitions, lattice
