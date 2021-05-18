b2804b863b34d1d429cffc732c856543db3cabce
Tom Mertens <your.email@whatev.er>
2021-05-18 19:31:58 +0200

    updated madtype exception

--- latticeconstructor/parse.py
+++ latticeconstructor/parse.py
@@ -13,6 +13,19 @@ from typing import Tuple, Union
 from latticejson.convert import FROM_ELEGANT, TO_MADX
 from latticejson.parse import parse_elegant, parse_madx
 
+_CONVERSION_DICT = {
+    "KQUAD": "QUADRUPOLE",
+    "KSEXT": "SEXTUPOLE",
+    "DRIF": "DRIFT",
+    "RFCA": "RFCAVITY",
+    "CSBEND": "SBEND",
+    "MONI": "MONITOR",
+    "WATCH": "MARKER",
+    "EVKICK": "VKICKER",
+    "EHKICK": "HKICKER",
+    "MARK": "MARKER",
+}
+
 
 def parse_from_string(string: str, ftype: str = "lte") -> Tuple[Union[str, None], dict, list]:
     """Method to parse an elegant lattice string to name, defintions and ordered
@@ -21,15 +34,15 @@ def parse_from_string(string: str, ftype: str = "lte") -> Tuple[Union[str, None]
     Parameters
     ----------
     string : str
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    Lattice string.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    Lattice string.
 
     ftype: optional, str, default = 'lte'
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    format type
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    format type
 
     Returns
     -------
     Tuple[Union[str,None],dict, list]
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    name, element definitions dict, element ordered list
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    name, element definitions dict, element ordered list
 
     """
     assert ftype.lower() in ["lte", "madx"]
@@ -60,7 +73,7 @@ def parse_from_string(string: str, ftype: str = "lte") -> Tuple[Union[str, None]
             try:
                 madtype = TO_MADX[FROM_ELEGANT[_type.lower()]].upper()
             except Exception:
-                madtype = "MARKER"
+                madtype = _CONVERSION_DICT[_type.upper()]
 
         definitions[name.upper()] = {
             **{"family": madtype},
